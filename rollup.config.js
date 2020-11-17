1f5720e8a496fc970cd341f87d7560c1f4eef4ea
Stephane Bernigaud <stephane@bernigaud.fr>
2020-11-17 11:08:18 +0100

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 export default {
   input: 'src/index.js',
   output: {
-    format: 'cjs',
+    format: 'es',
     file: 'lib/index.js',
-    exports: 'default',
   },
   external:  [ 'ra-core', 'query-string' ]
 };
