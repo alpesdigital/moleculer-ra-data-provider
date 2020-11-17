5ef02da2395b8ff843eaa75f11c2f996f8f07ee2
Stephane Bernigaud <stephane@bernigaud.fr>
2020-11-17 13:20:38 +0100

    re-build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,21 @@
+import resolve from "rollup-plugin-node-resolve"
+import babel from "rollup-plugin-babel"
+import commonjs from "rollup-plugin-commonjs"
+import pkg from "./package.json"
+
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: {
-    format: 'es',
-    file: 'lib/index.js',
+    dir: 'lib',
+    format: "cjs",
+    exports: 'auto'
   },
-  external:  [ 'ra-core', 'query-string' ]
-};
+  external: ["react", "ra-core", "query-string"],
+  plugins: [
+    babel({
+      exclude: "node_modules/**",
+    }),
+    resolve(),
+    commonjs(),
+  ],
+}
